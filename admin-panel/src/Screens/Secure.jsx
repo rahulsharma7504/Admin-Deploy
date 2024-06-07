f5118fac6cda59368b2a1dfea454cacd468769bc
Rahul Sharma <128781307+rahulsharma7504@users.noreply.github.com>
2024-06-07 16:34:23 +0530

    Update Secure.jsx

--- admin-panel/src/Screens/Secure.jsx
+++ admin-panel/src/Screens/Secure.jsx
@@ -1,3 +1,3 @@
-export const BaseURL='hhttps://admin-app-sjr8.onrender.com'
+export const BaseURL='https://admin-deploy-jx0j.onrender.com'
 // export const BaseURL='https://rotary3110.in'
 // export const BaseURL='http://localhost:4000'
